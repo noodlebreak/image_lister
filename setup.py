d8229952f20279e26b9903b5f2cd26fb17981eac
Ashish Kumar <ashish26kr91@gmail.com>
2015-12-12 17:32:57 +0530

    setup modified

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup  # ,find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -31,7 +31,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.8',
+    version='0.1.9',
 
     description='List images, with some basic statistics',
     long_description=long_description,
